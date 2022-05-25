47c8ddc5a40cde71768f3da2cc4fef18d0f65b12
SanjanaWuntakal <sanjanawuntakal@gmail.com>
2022-05-25 18:13:07 +0530

    first commit

---
+++ src/Data.jsx
@@ -0,0 +1,71 @@
+const DATA = [
+    {
+        id:0,
+        image:'https://th.bing.com/th/id/OIP.b1Hco_5YV66EckgZK9i2ngHaLX?w=201&h=309&c=7&r=0&o=5&dpr=1.5&pid=1.7',
+        title:'PATRICK MELROSE',
+        author:'Edward St auvyn',
+        price:450,
+    },
+    {
+        id : 1,
+        image:"https://www.bing.com/th?id=AMMS_6587b2a863d571e8d9477ab4414c7fa9&w=120&h=180&c=7&rs=1&qlt=80&o=6&cdv=1&dpr=1.5&pid=16.1",
+        title:'THE LAST COLLECTION',
+        author:'Jeanne Mackin',
+        price:100,
+    },
+    {
+        id : 2,
+        image:"https://www.bing.com/th?id=AMMS_f63a41569985f1d8023df61a6d4914f4&w=120&h=180&c=7&rs=1&qlt=80&o=6&cdv=1&dpr=1.5&pid=16.1",
+        title:'MOBY-DICK',
+        author:'Herman Melville',
+        price:200,
+       
+    },
+    {
+        id : 3,
+        image:"https://th.bing.com/th/id/OIP.egQihizlzvMcvvfJ7MY8yQHaLL?w=203&h=307&c=7&r=0&o=5&dpr=1.5&pid=1.7",
+       title:'DUNE',
+        author:'Frank Herbert',
+        price:300,
+        
+    },
+    {
+        id : 4,
+        image:"https://www.bing.com/th?id=AMMS_ba5d702642752d79b7d58be850110b46&w=120&h=180&c=7&rs=1&qlt=80&o=6&cdv=1&dpr=1.5&pid=16.1",
+        title:'SCARED GAMES',
+        author:'David Staurt Davies',
+        price:250,
+        
+    },
+    {
+        id : 5,
+        image:"https://www.bing.com/th?id=AMMS_b29b0efb8326a9dcee2f6fb027d82a8a&w=120&h=180&c=7&rs=1&qlt=80&o=6&cdv=1&dpr=1.5&pid=16.1",
+        title:'JANE EYRE',
+        author:'Magie Pearson',
+        price:200,
+        
+    },
+    {
+        id:6,
+        image:"https://www.bing.com/th?id=AMMS_7018c20e907e7193adb2252a3254967b&w=120&h=180&c=7&rs=1&qlt=80&o=6&cdv=1&dpr=1.5&pid=16.1",
+        title:'THE WAR AND PEACE',
+        author:'Leo Tolstoy',
+        price:700,
+    },
+    {
+        id:7,
+        image:"https://www.bing.com/th?id=AMMS_df50f2eaaca7bd2171fb24f12964f72e&w=120&h=180&c=7&rs=1&qlt=80&o=6&cdv=1&dpr=1.5&pid=16.1",
+        title:'THE BOOK THEIF',
+        author:'Markus Zusak',
+        price:200,
+    },
+    {
+        id:8,
+        image:"https://www.bing.com/th?id=AMMS_e91c588294c4557d8adf6e3ffb4e90bf&w=120&h=180&c=7&rs=1&qlt=80&o=6&cdv=1&dpr=1.5&pid=16.1",
+        title:'DON QUIXOTE',
+        author:'Miguel de Cervantes',
+        price:100,
+    }
+]
+
+export default DATA;
